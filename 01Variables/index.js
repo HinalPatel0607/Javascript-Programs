862b94c91eecd01a1c510b5a51f318dd2a62201e
Hinal <patel.hinal@outlook.com>
2024-03-11 17:14:37 +0530

    Variables and Datatypes

---
+++ 01Variables/index.js
@@ -0,0 +1,9 @@
+console.log("This contsins basic understanding of variables and datatypes")
+var a = 67;
+console.log(a);
+a = 'hinal'
+console.log(a);
+// We can change value of variable ar runtime an hence Javascript is dynamically typed language.
+//let 8abc = 9; //Not allowed in Javascript as variable declaration cannot be started with number
+// let var =8;// Not allowed as we cannot used reserved words to declare variables.
+
